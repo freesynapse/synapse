d16bdbbd8878fb11a8209daa874ed99be7ff6f65
freesynapse <mans.edstrom@gmail.com>
2023-12-29 00:04:32 +0100

    added float conversion of viewport resize events

--- synapse-core/SynapseCore/Event/RendererEvents.hpp
+++ synapse-core/SynapseCore/Event/RendererEvents.hpp
@@ -13,18 +13,21 @@ namespace Syn {
 	{
 	public:
 		ViewportResizeEvent(const glm::ivec2& _viewport) :
-			m_viewport(_viewport)
+			m_viewport(_viewport), m_viewportF(glm::vec2(_viewport.x, _viewport.y))
 		{}
 
-		inline const glm::ivec2& getViewport() { return m_viewport; }
+		inline const glm::ivec2 &getViewport() { return m_viewport; }
+		inline const glm::vec2 &getViewportF() { return m_viewportF; }
 		inline uint32_t getViewportX() { return m_viewport.x; }
 		inline uint32_t getViewportY() { return m_viewport.y; }
-
+		inline float getViewportXF() { return m_viewportF.x; }
+		inline float getViewportYF() { return m_viewportF.y; }
 
 		EVENT_TYPE(VIEWPORT_RESIZE);
 
 	private:
 		glm::ivec2 m_viewport;
+		glm::vec2 m_viewportF;
 	};
 
 
@@ -37,7 +40,6 @@ namespace Syn {
 		{}
 
 		inline const Ref<Shader> getShader() const { return m_shader; }
-
 
 		EVENT_TYPE(SHADER_RELOAD);
 
